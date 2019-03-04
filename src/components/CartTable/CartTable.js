72d9172b8d3c03c00008df6480a67a2389edaf27
Daniil <onejump9@gmail.com>
2019-03-04 14:00:38 +0300

    Refactoring reducer

--- src/components/CartTable/CartTable.js
+++ src/components/CartTable/CartTable.js
@@ -63,7 +63,7 @@ const CartTable = ({ items, total, onIncrease, onDecrease, onDelete }) => {
   );
 };
 
-const mapStateToProps = ({ cartItems, orderTotal }) => {
+const mapStateToProps = ({ shoppingCart: { cartItems, orderTotal } }) => {
   return {
     items: cartItems,
     total: orderTotal
